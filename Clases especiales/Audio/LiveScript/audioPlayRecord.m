b48d98f436a326f60323f1f31e1f9e25fc81cbb8
maxiyommi <maximilianoyommi@gmail.com>
2019-09-10 11:16:15 -0300

    actualizando clase especial y README.md

--- Clases especiales/Audio/LiveScript/audioPlayRecord.m
+++ Clases especiales/Audio/LiveScript/audioPlayRecord.m
@@ -1,9 +1,9 @@
-fileReader = dsp.AudioFileReader('xt.wav');
+fileReader = dsp.AudioFileReader('sinesweep.wav');
 
 deviceWriter = audioDeviceWriter();
     deviceWriter.SampleRate = 44100;
     deviceWriter.BitDepth = '16-bit integer';
-fileInfo = audioinfo('xt.wav')
+fileInfo = audioinfo('sinesweep.wav')
 
 setup(deviceWriter,zeros(fileReader.SamplesPerFrame,fileInfo.NumChannels));
 
@@ -15,14 +15,13 @@ deviceReader = audioDeviceReader;
 
 setup(deviceReader) ;% Función de MATLAB para autoconfigurar objetos y dismunir la carga computacional
 
-fileWriter = dsp.AudioFileWriter('nuevo.wav', 'FileFormat', 'WAV'); % Utilizo AudioFileWriter, objeto de la clase
+fileWriter = dsp.AudioFileWriter('sinesweep_mod.wav', 'FileFormat', 'WAV'); % Utilizo AudioFileWriter, objeto de la clase
     
 
 while ~isDone(fileReader)
     audioData = fileReader();
     audioFromDevice = deviceReader();
     deviceWriter(audioData);
-    scope(audioFromDevice)
     fileWriter(audioFromDevice);
 end
 
@@ -30,3 +29,5 @@ release(fileReader);
 release(deviceWriter); 
 release(deviceReader)
 release(fileWriter)
+close all
+clear all
--- README.md
+++ README.md
@@ -68,6 +68,8 @@ Las entregas y/o presentaciones se realizan en el dia y horario de las práctica
     * Funciones anidadas.
     * Encapsulado de funciones.
     * Ejercicio propuesto.
+* Clases especiales
+    - [Audio en MATLAB](https://htmlpreview.github.io/?https://github.com/maxiyommi/signal-systems/blob/master/Clases%20especiales/Audio/LiveScript/html/ClaseEspecial_Audio.html)
 
 ## Docentes
 
